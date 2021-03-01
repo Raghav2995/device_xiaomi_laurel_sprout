b1f2f1e47948637dbed5a6249ffc3f2ebde2d2db
Raghav2995 <raghavisbest2995@gmail.com>
2021-03-01 08:44:12 +0530

    Removed lineage overlays
    also removed some sepolicy since we are permissive now

--- device.mk
+++ device.mk
@@ -68,11 +68,7 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
-
-PRODUCT_PACKAGES += \
-    NoCutoutOverlay
+    $(LOCAL_PATH)/overlay 
 
 # Properties
 -include $(LOCAL_PATH)/system_prop.mk
